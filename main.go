a65e14fd97a106e5640ce855acb867c8f3730b07
Reidar Sollid <reidarsollid@Reidars-MacBook-Pro.local>
2020-09-01 20:20:24 +0200

    Corrected imports

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	c "crosscalc/crisscross"
-	slices "crosscalc/slices"
+
 	"fmt"
+	c "github.com/reidarsollid/crosscalc/crisscross"
+	"github.com/reidarsollid/crosscalc/slices"
 )
 
 const txt = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08\n" +
